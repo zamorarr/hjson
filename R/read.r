e8a0dce7324c32e5c188a60322504aff16cab6f4
Robert Zamora <zamora.rr@gmail.com>
2022-02-11 09:41:59 -0500

    normalize path

--- R/read.r
+++ R/read.r
@@ -4,6 +4,7 @@
 #' @param ... params passed to jsonlite::fromJSON
 #' @export
 read_hjson <- function(path, ...) {
+  path <- normalizePath(path, mustWork = TRUE)
   jsonstr <- read_hjson_cpp(path)
   jsonlite::fromJSON(jsonstr, ...)
 }
